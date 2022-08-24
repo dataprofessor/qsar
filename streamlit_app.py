f5e219ea2fc1615a9d9c6a41369348c6d3d007c8
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-08-24 11:13:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,8 @@ df = pd.read_csv('data/hcv_ns5b_curated_data.csv')
 st.write(df)
 
 example_input = "CC(=O)OC1=CC=CC=C1C(=O)O"
+st.write(example_input)
 
-smiles_txt = st.text_input('Enter SMILES notation', example_input)
+smiles_txt = st.text_input('Enter SMILES notation', '')
 st.info(smiles_txt)
       
