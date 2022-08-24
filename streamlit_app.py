4e35cac1e1fa1079b7a2df4c7a533d642a46da1e
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-08-24 23:42:52 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -76,7 +76,7 @@ if st.session_state.smiles_input != '':
     with st.expander('Show molecular structure via stmol'):
       f2 = open('molecule.xyz', 'r')
       molecule_xyz = f2.read()
-      speck_plot(molecule_xyz, wbox_width='550px')
+      speck_plot(molecule_xyz, wbox_width='350px')
 
 
 # Compute PADEL descriptors
