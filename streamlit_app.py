cdf31781e2cdde0c39c4ce1496bdab965eb6aa18
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-08-24 11:02:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,12 @@
 import streamlit as st
 import pandas as pd
+from padelpy import padeldescriptor
 
 st.title('💊 Bioactivity prediction app')
 
 df = pd.read_csv('data/hcv_ns5b_curated_data.csv')
-
 st.write(df)
+
+smiles_txt = st.text_input('Enter SMILES notation', 'CC(=O)OC1=CC=CC=C1C(=O)O')
+st.info(smiles_txt)
+      
