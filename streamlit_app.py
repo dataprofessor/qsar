d35ff91b3e432ecc57c7d14056c0517da21ed4bb
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-08-24 11:23:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,9 +9,6 @@ ic50 = pd.read_csv('data/hcv_ns5b_ic50_nm.csv', header=None)
 st.write(molecule)
 st.write(ic50)
 
-example_input = "CC(=O)OC1=CC=CC=C1C(=O)O"
-#st.write(example_input)
-
-smiles_txt = st.text_input('Enter SMILES notation', '')
+example_input = 'CC(=O)OC1=CC=CC=C1C(=O)O'
+smiles_txt = st.text_input('Enter SMILES notation', example_input)
 st.info(smiles_txt)
-      
