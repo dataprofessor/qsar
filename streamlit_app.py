cbea0ef9da2c5296f6f8c8bad2fbcc2e4ed7fc13
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-08-24 14:10:20 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ st.set_page_config(
   initial_sidebar_state='collapsed')
 
 if 'example_input' not in st.session_state:
-  st.session_state.example_input = ''
+  st.session_state.smiles_input = ''
 
 if os.path.isfile('molecule.smi'):
   os.remove('molecule.smi') 
@@ -22,18 +22,18 @@ st.title('💊 Bioactivity prediction app')
 st.sidebar.subheader('Input SMILES')
 
 def insert_example_smiles():
-    st.session_state.example_input = 'CC(=O)OC1=CC=CC=C1C(=O)O'
+    st.session_state.smiles_input = 'CC(=O)OC1=CC=CC=C1C(=O)O'
 def clear_smiles():
-    st.session_state.example_input = ''
+    st.session_state.smiles_input = ''
 
-smiles_txt = st.sidebar.text_input('Enter SMILES notation', st.session_state.example_input)
+smiles_txt = st.sidebar.text_input('Enter SMILES notation', st.session_state.smiles_input)
 st.sidebar.button('Example input', on_click=insert_example_smiles)
 st.sidebar.button('Clear input', on_click=clear_smiles)
 
 
 st.subheader('Status')
 
-if st.session_state.example_input == '':
+if st.session_state.smiles_input == '':
   st.info('Enter SMILES notation in the sidebar to proceed', icon='👈')
 else:
   st.info(smiles_txt, icon='💊')
@@ -44,20 +44,20 @@ f.close()
 
 
 # Compute PADEL descriptors
-st.subheader('🔢 Descriptors')
+if st.session_state.smiles_input is not '':
+  st.subheader('🔢 Descriptors')
+  if os.path.isfile('molecule.smi'):
+    padeldescriptor(mol_dir='molecule.smi', 
+                    d_file='descriptors.csv',
+                    descriptortypes='data/PubchemFingerprinter.xml', 
+                    detectaromaticity=True,
+                    standardizenitro=True,
+                    standardizetautomers=True,
+                    threads=2,
+                    removesalt=True,
+                    log=True,
+                    fingerprints=True)
 
-if os.path.isfile('molecule.smi'):
-  padeldescriptor(mol_dir='molecule.smi', 
-                  d_file='descriptors.csv',
-                  descriptortypes='data/PubchemFingerprinter.xml', 
-                  detectaromaticity=True,
-                  standardizenitro=True,
-                  standardizetautomers=True,
-                  threads=2,
-                  removesalt=True,
-                  log=True,
-                  fingerprints=True)
-
-descriptors = pd.read_csv('descriptors.csv')
-descriptors.drop('Name', axis=1, inplace=True)
-st.write(descriptors)
+  descriptors = pd.read_csv('descriptors.csv')
+  descriptors.drop('Name', axis=1, inplace=True)
+  st.write(descriptors)
