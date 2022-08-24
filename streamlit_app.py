cba88d393ec9637c996b99d67aa7dc3e400e6c0f
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-08-24 11:29:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,9 +6,27 @@ st.title('💊 Bioactivity prediction app')
 
 molecule = pd.read_csv('data/molecule.smi', header=None)
 ic50 = pd.read_csv('data/hcv_ns5b_ic50_nm.csv', header=None)
-st.write(molecule)
-st.write(ic50)
 
 example_input = 'CC(=O)OC1=CC=CC=C1C(=O)O'
 smiles_txt = st.text_input('Enter SMILES notation', example_input)
 st.info(smiles_txt)
+
+
+fingerprint = 'Substructure'
+
+fingerprint_output_file = ''.join([fingerprint,'.csv']) #Substructure.csv
+fingerprint_descriptortypes = fp[fingerprint]
+
+padeldescriptor(mol_dir='molecule.smi', 
+                d_file='descriptors.csv',
+                descriptortypes='data/PubchemFingerprinter.xml', 
+                detectaromaticity=True,
+                standardizenitro=True,
+                standardizetautomers=True,
+                threads=2,
+                removesalt=True,
+                log=True,
+                fingerprints=True)
+
+descriptors = pd.read_csv('descriptors.csv')
+st.write(descriptors)
