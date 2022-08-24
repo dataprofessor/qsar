bc0754e1d94688cb955ec951b280eb5041cc1629
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2022-08-24 19:36:52 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,18 +53,14 @@ if st.session_state.smiles_input != '':
   AllChem.MMFFOptimizeMolecule(m2)
   m3 = Chem.MolToXYZBlock(m2)
   Chem.MolToXYZFile(m2, 'molecule.xyz')
-  
-  col1, col2 = st.columns(2)
-  
-  with col1:
-    with st.expander('Show XYZ file content'):
-      st.code(m3)
-      
-  with col2:
-    with st.expander('Show molecular structure via speck_plot'):
-      f2 = open('molecule.xyz', 'r')
-      molecule_xyz = f2.read()
-      speck_plot(molecule_xyz, wbox_width='200px')
+
+  with st.expander('Show XYZ file content'):
+    st.code(m3)
+
+  with st.expander('Show molecular structure via speck_plot'):
+    f2 = open('molecule.xyz', 'r')
+    molecule_xyz = f2.read()
+    speck_plot(molecule_xyz, wbox_width='200px')
 
 
 # Compute PADEL descriptors
